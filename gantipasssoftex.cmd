94fc7be60e05f96136ad8e73e639b92d8f70df22
cmaimu <cmdimu@gmail.com>
2014-10-13 14:03:17 +0700

    Create gantipasssoftex.cmd

---
+++ gantipasssoftex.cmd
@@ -0,0 +1,10 @@
+1. Ketik: cd /usr/local/vpnserver
+2. ketik: ./vpncmd
+3. pilih: 1
+4. masukkan host dan portnya (format host:port)= 192.168.0.1:5555
+5. ada specifi virtual hub name,langsung aja enter
+6. masukkan password sekarang
+7. ketik: ServerPasswordSet
+8. Masukkan password baru 2 kali
+9.. Selesai.
+10. untuk keluar, klik CTRL+C
